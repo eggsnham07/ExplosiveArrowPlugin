4983a4c0214cab90552351ebd5d1dd219fd0dc0a
eggsnham07 <eggsnham07@gmail.com>
2021-07-11 12:08:36 -0600

    Added/Updated Files

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Users\eggsn\Documents\java\ExplosiveArrowPlugin
-mvn package
+mvn package && xcopy target\ExplosiveArrowPlugin-1.0-SNAPSHOT.jar C:\Users\eggsn\Desktop\world_1\plugins\ExplosiveArrowPlugin.jar
