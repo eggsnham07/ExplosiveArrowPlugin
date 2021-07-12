78899f1d66802ab941c121a8871dbbb4019e2a6c
eggsnham07 <eggsnham07@gmail.com>
2021-07-11 19:23:47 -0600

    Added/Updated Files

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Users\eggsn\Documents\java\ExplosiveArrowPlugin
-mvn package && xcopy C:\Users\eggsn\Documents\java\ExplosiveArrowPlugin\target\ExplosiveArrowPlugin-%1-SNAPSHOT.jar C:\Users\eggsn\Desktop\world_1\plugins\ExplosiveArrowPlugin.jar
+mvn package && xcopy %HOMEDRIVE%%HOMEPATH%\Documents\java\ExplosiveArrowPlugin\target\ExplosiveArrowPlugin-%1-SNAPSHOT.jar %HOMEDRIVE%%HOMEPATH%\Desktop\world_1\plugins\ExplosiveArrowPlugin.jar
